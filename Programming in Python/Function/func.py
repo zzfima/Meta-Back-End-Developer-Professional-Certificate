47ebfa9e4ea8fba86db7df9d7263750ce87a6e1b
Efim Zabarsky <EZ>
2024-05-01 20:46:13 +0300

    continue

--- Programming in Python/Function/func.py
+++ Programming in Python/Function/func.py
@@ -1,6 +1,42 @@
 def get_total(a, b):
-    #local variable declared inside a function
+    # local variable declared inside a function
     total = a + b
     return total
 
+
 print("5 + 2 = ", get_total(5, 2))
+# print(total)
+
+
+# Enclosing scope
+def get_total(a, b):
+    # enclosed variable declared inside a function
+    total = a + b
+
+    def double_it():
+        # local variable
+        double = total * 2
+        print(double)
+
+    double_it()
+
+    return total
+
+
+# Global scope
+special = 5
+
+
+def get_total(a, b):
+    # enclosed scope variable declared inside a function
+    total = a + b
+    print(special)
+
+    def double_it():
+        # local variable
+        double = total * 2
+        print(special)
+
+    double_it()
+
+    return total
