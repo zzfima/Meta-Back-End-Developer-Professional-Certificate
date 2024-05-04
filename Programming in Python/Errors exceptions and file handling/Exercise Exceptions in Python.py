59b2513abf65e4285031a33a1390e341b4fbc36c
Efim Zabarsky <EZ>
2024-05-04 11:34:39 +0300

    finish exercise

--- Programming in Python/Errors exceptions and file handling/Exercise Exceptions in Python.py
+++ Programming in Python/Errors exceptions and file handling/Exercise Exceptions in Python.py
@@ -1,4 +1,4 @@
-# Starter code
+# IndexError
 items = [1, 2, 3, 4, 5]
 try:
     item = items[6]
@@ -7,3 +7,23 @@ except IndexError:
     print("Item does not exist in the list")
 except Exception as e:
     print(e, " something goes wrong")
+
+
+# ZeroDivisionError
+def divide_by(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+
+
+ans = divide_by(40, 0)
+print(ans)
+
+
+# FileNotFoundError
+try:
+    with open("file_does_not_exist.txt", "r") as file:
+        print(file.read())
+except FileNotFoundError:
+    print("The file could not be found")
