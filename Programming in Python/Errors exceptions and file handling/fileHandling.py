1f6087d8942a8d57490cf163e88d26832abcbd23
Efim Zabarsky <EZ>
2024-05-09 15:43:42 +0300

    rfct

--- Programming in Python/Errors exceptions and file handling/fileHandling.py
+++ Programming in Python/Errors exceptions and file handling/fileHandling.py
@@ -1,2 +1,5 @@
-with open("text.txt", mode="r") as txtFile:
+with open("test.txt", mode="w") as fileForWrite:
+    fileForWrite.write("Hello python!")
+
+with open("test.txt", mode="r") as txtFile:
     print(txtFile.read())
