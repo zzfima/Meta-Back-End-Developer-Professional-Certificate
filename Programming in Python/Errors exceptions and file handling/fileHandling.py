55a64c3ee5e2dd44f102a1d368f1a2af7df004b9
zzfima <zzfima@gmail.com>
2024-05-13 10:44:43 +0300

    add different read methods

--- Programming in Python/Errors exceptions and file handling/fileHandling.py
+++ Programming in Python/Errors exceptions and file handling/fileHandling.py
@@ -1,5 +1,17 @@
 with open("test.txt", mode="w") as fileForWrite:
-    fileForWrite.write("Hello python!")
+    fileForWrite.writelines(["Hello python!", "\nIts Me!"])
 
+print("\n*****read****")
 with open("test.txt", mode="r") as txtFile:
     print(txtFile.read())
+
+print("\n*****readline****")
+with open("test.txt", mode="r") as txtFile:
+    print(txtFile.readline())
+
+print("\n****readlines*****")
+cnt = 0
+with open("test.txt", mode="r") as txtFile:
+    for l in txtFile.readlines():
+        print(cnt, l)
+        cnt += 1
