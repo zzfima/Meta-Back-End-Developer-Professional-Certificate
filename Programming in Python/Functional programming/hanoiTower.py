4381522aabe09ca84a1446f137530c71dab8fb4e
Efim Zabarsky <EZ>
2024-05-15 15:49:43 +0300

    done

---
+++ Programming in Python/Functional programming/hanoiTower.py
@@ -0,0 +1,27 @@
+# Recursive function for Tower of Hanoi
+def hanoi(disks: int, source: str, helper: str, destination: str):
+    # Base Condition
+    if disks == 1:
+        print(
+            "Disk {} moves from tower {} to tower {}.".format(
+                disks, source, destination
+            )
+        )
+        return
+
+    # Recursive calls in which function calls itself
+    hanoi(disks - 1, source, destination, helper)
+    print("Disk {} moves from tower {} to tower {}.".format(disks, source, destination))
+    hanoi(disks - 1, helper, source, destination)
+
+
+# Driver code
+disks = int(input("Number of disks to be displaced: "))
+"""
+Tower names passed as arguments:
+Source: A
+Helper: B
+Destination: C
+"""
+# Actual function call
+hanoi(disks, "A", "B", "C")
