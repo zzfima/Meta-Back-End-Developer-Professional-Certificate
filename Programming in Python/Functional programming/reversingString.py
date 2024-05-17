42ceba1a34079d227922a4927fa7c398209a90e6
Efim Zabarsky <EZ>
2024-05-17 17:42:14 +0300

    rfct

--- Programming in Python/Functional programming/reversingString.py
+++ Programming in Python/Functional programming/reversingString.py
@@ -1,5 +1,15 @@
 def reverseString(s: str) -> str:
     return s[::-1]
 
-print('*********')
+
+def reverseStringRecursive(s: str) -> str:
+    if len(s) == 0:
+        return s
+    return reverseStringRecursive(s[1:]) + s[0]
+
+
+print("*********")
 print(reverseString("abcdef"))
+
+print("*********")
+print(reverseStringRecursive("abcdef"))
