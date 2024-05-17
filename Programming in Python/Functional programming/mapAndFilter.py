b5f3bbb1b46d6e37d942aa654eaf64e19c8c903a
zzfima <zzfima@gmail.com>
2024-05-17 08:59:10 +0300

    add filter

--- Programming in Python/Functional programming/mapAndFilter.py
+++ Programming in Python/Functional programming/mapAndFilter.py
@@ -5,9 +5,14 @@ def checkSign(i: int) -> str:
         return "neg: " + str(i)
 
 
+# arrange values with their polarity
 addedList = map(checkSign, [1, 2, -3, 4, -5])
 print(*addedList)
 
-
+# return all numbers multiplicated by 20
 addedList = map(lambda i: i * 10, [1, 2, -3, 4, -5])
+print(*addedList)
+
+# take only positive numbers
+addedList = filter(lambda i: i >= 0, [1, 2, -3, 4, -5])
 print(*addedList)
