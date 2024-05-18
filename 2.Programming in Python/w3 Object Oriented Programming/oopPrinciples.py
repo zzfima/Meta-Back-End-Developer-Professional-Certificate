7b65aa7cdc0c60ed16449ef653c045dceb439443
zzfima <zzfima@gmail.com>
2024-05-18 10:01:19 +0300

    add class types: public protected private

---
+++ 2.Programming in Python/w3 Object Oriented Programming/oopPrinciples.py
@@ -0,0 +1,25 @@
+from abc import ABC
+
+
+class Engine(ABC):
+    def __init__(self):
+        self._a = 2  # protected
+        self.__b = 3  # private
+
+
+class Diesel(Engine):
+    def __init__(self):
+        super().__init__()
+        self.k = 4
+
+    def printState(self):
+        print(self.k)
+        print(self._a)
+        # cannot access private: print(self.__b)
+
+
+d = Diesel()
+d.printState()
+print(d.k)
+print(d._a)
+# cannot access private: print(d.__b)
