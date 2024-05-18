916f9853216144d944616722714b2287bb978142
Efim Zabarsky <EZ>
2024-05-18 16:13:18 +0300

    done

---
+++ 2.Programming in Python/w3 Object Oriented Programming/Exercise Instantiate a custom Object.py
@@ -0,0 +1,11 @@
+class MyFirstClass:
+    def __init__(self) -> None:
+        print("Who wrote this?")
+        self.index = "Author-Book"
+
+    def hand_list(self, philosopher: str, book: str):
+        print("{} wrote this book: {}".format(philosopher, book))
+
+
+c = MyFirstClass()
+c.hand_list("Plato", "Republic")
