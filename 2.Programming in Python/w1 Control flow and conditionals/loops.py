bc162657bb99f479e8af2c13d6ceacf1e35a884c
zzfima <zzfima@gmail.com>
2024-05-18 07:31:55 +0300

    add continue

--- 2.Programming in Python/w1 Control flow and conditionals/loops.py
+++ 2.Programming in Python/w1 Control flow and conditionals/loops.py
@@ -1,15 +1,19 @@
+print("**1**")
 text = "hello"
-
 for c, i in enumerate(text):
     print("element at ", c, " is character ", i)
 
+print("**2**")
 cnt = 0
-
 while cnt < 5:
+    if cnt == 1:
+        cnt = 2
+        continue
     print(cnt)
     if cnt == 2:
         break
     cnt += 1
 
+print("**3**")
 for i in range(5):
     print(i)
