d7795bb8bafdea1644e2d078a1424b670e39ade0
Efim Zabarsky <EZ>
2024-05-21 09:43:02 +0300

    finished

---
+++ 2.Programming in Python/w3 Object Oriented Programming/Abstract classes and methods.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+
+class EmployeeBase(ABC):
+    @abstractmethod
+    def printName(self):
+        pass
+
+
+class Boss(EmployeeBase):
+    def printName(self):
+        print("I am a boss")
+
+class Cleaner(EmployeeBase):
+    def printName(self):
+        print("I am a cleaner")
+        
+        
+def employeePrinter(e: EmployeeBase):
+    e.printName()
+    
+print('')    
+e1 = Boss()
+employeePrinter(e1)
+e2 = Cleaner()
+employeePrinter(e2)
