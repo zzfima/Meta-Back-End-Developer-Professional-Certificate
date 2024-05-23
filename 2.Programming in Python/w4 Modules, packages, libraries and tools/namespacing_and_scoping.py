afc10dcebf8b96bb7582f893fef84c7850f269dd
Efim Zabarsky <EZ>
2024-05-23 16:49:22 +0300

    add nonlocal

--- 2.Programming in Python/w4 Modules, packages, libraries and tools/namespacing_and_scoping.py
+++ 2.Programming in Python/w4 Modules, packages, libraries and tools/namespacing_and_scoping.py
@@ -1,15 +1,34 @@
 # LGBE
 
 # Global
-trm = "lilo"
-print("Global var: {}".format(trm))
+var_g = "var global"
+var_nl = "var nl"
 
 
-class scopes:
-    pass
+def fn_out():
+    var_nl1 = "fsd1"
 
-    def f1(self):
-        # local
-        v = 2 + 2
-        k = trm
-        print(v, k)
+    def fnc_local():
+        # Local
+        var_l = "vr local"
+        print("Local var: {}".format(var_l))
+        nonlocal var_nl1
+        var_nl1 = "fsd2"
+
+    print("fn_out var: {}".format(var_nl1))
+    fnc_local()
+    print("fn_out var: {}".format(var_nl1))
+
+
+def fnc_enclosed():
+    # Enclosed
+    var_g = "var enclosed"
+    print("Enclosed var: {}".format(var_g))
+
+
+print("Global var nl: {}".format(var_nl))
+print("Global var: {}".format(var_g))
+fn_out()
+fnc_enclosed()
+print("Global var: {}".format(var_g))
+print("Global var nl: {}".format(var_nl))
